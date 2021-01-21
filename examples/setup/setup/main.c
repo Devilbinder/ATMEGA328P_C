e5f4cbb01f0e90184e99cd73b7b3f53e6e851c03
Devilbinder <Devilbinder@users.noreply.github.com>
2021-01-21 21:41:34 +0200

    update license

--- examples/setup/setup/main.c
+++ examples/setup/setup/main.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 Devilbinder (Binder Tronics)
+Copyright (c) 2020 Devilbinder (Binder Tronics) https://bindertronics.com/
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
